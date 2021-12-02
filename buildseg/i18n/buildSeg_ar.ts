bea6940c4b7d33b2928c659af12db2d79d3817df
Youssef-Harby <youssef_harby@yahoo.com>
2021-12-02 18:51:56 +0200

    Multi Language support

---
+++ buildseg/i18n/buildSeg_ar.ts
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ar">
+<context>
+    <name>buildSegDialogBase</name>
+    <message>
+        <location filename="../buildSeg_dialog_base.ui" line="32"/>
+        <source>buildSeg</source>
+        <translation>buildSeg</translation>
+    </message>
+    <message>
+        <location filename="../buildSeg_dialog_base.ui" line="60"/>
+        <source>Model name</source>
+        <translation>أسم النموذج</translation>
+    </message>
+    <message>
+        <location filename="../buildSeg_dialog_base.ui" line="70"/>
+        <location filename="../buildSeg_dialog_base.ui" line="74"/>
+        <source>Unet</source>
+        <translation>Unet</translation>
+    </message>
+    <message>
+        <location filename="../buildSeg_dialog_base.ui" line="86"/>
+        <source>Parameter path</source>
+        <translation>مسار النموذج</translation>
+    </message>
+    <message>
+        <location filename="../buildSeg_dialog_base.ui" line="96"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+</context>
+</TS>
