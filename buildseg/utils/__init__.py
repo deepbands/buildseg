cbc47a6d43f6bc55a91216273eb5252ea16a223e
geoyee <geoyee@yeah.net>
2022-03-17 16:40:58 +0800

    Test submodel

--- buildseg/utils/__init__.py
+++ buildseg/utils/__init__.py
@@ -4,3 +4,4 @@ from .convert import layer2array
 from .splicing import create_grids, Mask  # splicing_grids
 from .shape import polygonize_raster
 from .simplify import simplify_polygon, dowm_sample
+from .tiles2tiff import get_raster_from_titles
